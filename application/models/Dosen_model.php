faf90a81058114699a9248bbbac2f34b5e451c0e
kristt26 <kristt26@gmail.com>
2019-11-11 22:07:12 +0900

    New 1

--- application/models/Dosen_model.php
+++ application/models/Dosen_model.php
@@ -4,7 +4,16 @@ class Dosen_Model extends CI_Model
 {
     public function get()
     {
-        $result = $this->db->get("dosen");
+        $result = $this->db->query("
+            SELECT
+                *
+            FROM
+                `dosen`
+                RIGHT JOIN `pegawai` ON `dosen`.`idpegawai` = `pegawai`.`idpegawai` 
+            WHERE 
+                `dosen`.nidn!='null' AND
+                `dosen`.scholarId!='null'
+        ");
         return $result->result_array();
     }    
 }
