8f99874b84b7d1c269f291e201834ab3fe416055
kristt26 <kristt26@gmail.com>
2019-11-28 19:34:31 +0900

    12

--- application/controllers/api/BeritaAcara.php
+++ application/controllers/api/BeritaAcara.php
@@ -20,36 +20,30 @@ class BeritaAcara extends \Restserver\Libraries\REST_Controller
         header("Access-Control-Allow-Origin: *");
         header("Content-Type: application/json; charset=UTF-8");
         header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-        if ( "OPTIONS" === $_SERVER['REQUEST_METHOD'] ) {
-            $message = [
-                "data" => true
-            ];
-            $this->response($message, REST_Controller::HTTP_OK);
-        }else{
-            $this->load->library('Authorization_Token');
-            $is_valid_token = $this->authorization_token->validateToken();
-            $data = json_decode($this->security->xss_clean($this->input->raw_input_stream), true);
-            if ($is_valid_token['status'] === true) {
-                $Output = $this->BeritaAcaraModel->insert($data);
-                if ($Output > 0) {
-                    $message = [
-                        "data" => $Output,
-                    ];
-                    $this->response($message, REST_Controller::HTTP_OK);
-                } else {
-                    $message = [
-                        "data" => $Output,
-                    ];
-                    $this->response($message, REST_Controller::HTTP_OK);
-                }
+        $this->load->library('Authorization_Token');
+        $is_valid_token = $this->authorization_token->validateToken();
+        $data = json_decode($this->security->xss_clean($this->input->raw_input_stream), true);
+        if ($is_valid_token['status'] === true) {
+            $Output = $this->BeritaAcaraModel->insert($data);
+            if ($Output > 0) {
+                $message = [
+                    "data" => $Output,
+                ];
+                $this->response($message, REST_Controller::HTTP_OK);
             } else {
                 $message = [
-                    "data" => "Session anda telah habis",
+                    "data" => $Output,
                 ];
-                $this->response($message, REST_Controller::HTTP_NOT_FOUND);
+                $this->response($message, REST_Controller::HTTP_OK);
             }
+        } else {
+            $message = [
+                "data" => "Session anda telah habis",
+            ];
+            $this->response($message, REST_Controller::HTTP_NOT_FOUND);
         }
     }
+
     public function LaporanBa_get()
     {
         header("Access-Control-Allow-Origin: *");
