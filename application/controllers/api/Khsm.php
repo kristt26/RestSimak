9584c850af50ab6a69e5c7f2dd6f1b7567082934
kristt26 <kristt26@gmail.com>
2020-01-27 15:00:17 +0900

    New3

--- application/controllers/api/Khsm.php
+++ application/controllers/api/Khsm.php
@@ -9,7 +9,7 @@ class Khsm extends \Restserver\Libraries\REST_Controller
 {
     public function __construct($config = 'rest')
     {
-        parent::__construct();
+        parent::__construct($config);
         header("Access-Control-Allow-Origin: *");
         header("Content-Type: application/json; charset=UTF-8");
         header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT, OPTIONS");
