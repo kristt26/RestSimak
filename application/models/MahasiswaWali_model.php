f47ebad318c41cce30311e2c1d820c50c9c8d977
kristt26 <kristt26@gmail.com>
2020-02-07 17:34:42 +0900

    New Wali

--- application/models/MahasiswaWali_model.php
+++ application/models/MahasiswaWali_model.php
@@ -15,31 +15,33 @@ class MahasiswaWali_model extends CI_Model
             `mahasiswa`.`jk`,
             `program_studi`.`kdps`,
             `program_studi`.`nmps`,
-            `program_studi`.`jenjang`
+            `program_studi`.`jenjang`,
+            (SELECT
+            SUM(CASE
+            WHEN transkip.ket= 'L' THEN 1*transkip.nxsks
+            ELSE
+            0*transkip.nxsks
+            END)/SUM(CASE
+            WHEN transkip.ket = 'L' THEN 1*transkip.sks
+            ELSE
+            0*transkip.sks
+            END) FROM transkip WHERE transkip.npm=`mahasiswa`.`npm`)
+            AS IPK ,
+
+            (SELECT
+            SUM(CASE
+            WHEN transkip.ket = 'L' THEN 1*transkip.sks
+            ELSE
+            0*transkip.sks
+            END)FROM transkip WHERE transkip.npm=`mahasiswa`.`npm`) AS SKSLulus
           FROM
             `dosen_wali`
             LEFT JOIN `mahasiswa` ON `dosen_wali`.`npm` = `mahasiswa`.`npm`
             RIGHT JOIN `program_studi` ON `mahasiswa`.`kdps` = `program_studi`.`kdps`
             RIGHT JOIN `dosen` ON `dosen`.`iddosen` = `dosen_wali`.`iddosen`
             RIGHT JOIN `pegawai` ON `pegawai`.`idpegawai` = `dosen`.`idpegawai`
-          WHERE mahasiswa.statuskul IN('AKTIF','TIDAK AKTIF','CUTI') AND pegawai.IdUser='$IdUser'");
-        $data = $result->result_object();
-        foreach ($data as $key => $value) {
-            $query = $this->db->query("CALL GetIPK('$value->npm')");
-            $res = $query->result();
-            //add this two line
-            $query->next_result();
-            $query->free_result();
-            //end of new code
-            $value->IPK = $res[0]->IPK;
-            $value->SKSLulus = $res[0]->SKSLulus;
-            $qSemester = $this->db->query("CALL SemesterMahasiswa('$value->npm')");
-            $xSemester = $qSemester->result();
-            $query->next_result();
-            $query->free_result();
-            $value->Semester = $xSemester[0]->Semester;
-
-        }
-        return $data;
+          WHERE mahasiswa.statuskul IN('AKTIF','TIDAK AKTIF','CUTI') AND pegawai.IdUser='$IdUser'"
+        );
+        return $result->result_object();
     }
 }
