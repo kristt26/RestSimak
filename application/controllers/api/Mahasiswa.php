e7b4fb5c1dec6929f64483146d6426c8b6d4515e
kristt26 <kristt26@gmail.com>
2020-07-11 15:03:03 +0900

    adfa

--- application/controllers/api/Mahasiswa.php
+++ application/controllers/api/Mahasiswa.php
@@ -58,23 +58,23 @@ class Mahasiswa extends \Restserver\Libraries\REST_Controller
         }
     }
 
-    // public function DataMahasiswa_get()
-    // {
-    //     $Output = $this->MahasiswaModel->GetMahasiswa($npm);
-    //     if (!empty($Output)) {
-    //         $message = [
-    //             'status' => true,
-    //             'data' => $Output['data'],
-    //             'message' => "Success!",
-    //         ];
-    //         $this->response($message, REST_Controller::HTTP_OK);
-    //     } else {
-    //         $message = [
-    //             'status' => false,
-    //             'data' => [],
-    //             'message' => "Kosong",
-    //         ];
-    //         $this->response($message, REST_Controller::HTTP_NOT_FOUND);
-    //     }
-    // }
+    public function DataMahasiswa_get()
+    {
+        $Output = $this->MahasiswaModel->GetMahasiswa($npm);
+        if (!empty($Output)) {
+            $message = [
+                'status' => true,
+                'data' => $Output['data'],
+                'message' => "Success!",
+            ];
+            $this->response($message, REST_Controller::HTTP_OK);
+        } else {
+            $message = [
+                'status' => false,
+                'data' => [],
+                'message' => "Kosong",
+            ];
+            $this->response($message, REST_Controller::HTTP_NOT_FOUND);
+        }
+    }
 }
