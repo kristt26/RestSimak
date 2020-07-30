03b71d12f2a2e140e972f1771b9fc4d74071f224
kristt26 <kristt26@gmail.com>
2020-07-30 22:47:47 +0900

    fix

--- application/models/v2/Mahasiswa_model.php
+++ application/models/v2/Mahasiswa_model.php
@@ -29,10 +29,6 @@ class Mahasiswa_Model extends CI_Model
                 `mahasiswa`.`nmayah`,
                 `mahasiswa`.`almtayah`,
                 `mahasiswa`.`nmibu`,
-                `mahasiswa`.`sumbiaya`,
-                `mahasiswa`.`statuskul`,
-                `mahasiswa`.`tgdaftar`,
-                `mahasiswa`.`kurikulum`,
                 (SELECT SUM(CASE 
                     WHEN ket= 'L' THEN 1*nxsks
                     ELSE
@@ -98,10 +94,6 @@ class Mahasiswa_Model extends CI_Model
             `mahasiswa`.`nmayah`,
             `mahasiswa`.`almtayah`,
             `mahasiswa`.`nmibu`,
-            `mahasiswa`.`sumbiaya`,
-            `mahasiswa`.`statuskul`,
-            `mahasiswa`.`tgdaftar`,
-            `mahasiswa`.`kurikulum`,
             (SELECT SUM(CASE 
                 WHEN ket= 'L' THEN 1*nxsks
                 ELSE
@@ -125,106 +117,6 @@ class Mahasiswa_Model extends CI_Model
             `mahasiswa`.`statuskul` IN ('AKTIF') AND
             `tahun_akademik`.`status` = 'Aktif'
             GROUP BY `mahasiswa`.`npm`");
-            if ($ResultMahasiswa->num_rows()) {
-                $data = [
-                    'status' => true,
-                    'data' => $ResultMahasiswa->result(),
-                    'message' => "Success",
-                ];
-                return $data;
-            } else {
-                $data = [
-                    'status' => true,
-                    'data' => $ResultMahasiswa->result(),
-                    'message' => "Tidak Data Mahasiswa",
-                ];
-                return $data;
-            }
-        }
-    }
-    public function MahasiswaPublick($npm)
-    {
-        if ($npm != null && $npm!=="undefined") {
-            $ResultMahasiswa = $this->db->query("
-            SELECT
-                `mahasiswa`.`npm`,
-                `mahasiswa`.`kdps`,
-                `mahasiswa`.`jenjang`,
-                `mahasiswa`.`kelas`,
-                `mahasiswa`.`nmmhs`,
-                `mahasiswa`.`tmlhr`,
-                `mahasiswa`.`tglhr`,
-                `mahasiswa`.`jk`,
-                `mahasiswa`.`agama`,
-                `mahasiswa`.`kewarga`,
-                `mahasiswa`.`pendidikan`,
-                `mahasiswa`.`nmsmu`,
-                `mahasiswa`.`jursmu`,
-                `mahasiswa`.`kotasmu`,
-                `mahasiswa`.`kabsmu`,
-                `mahasiswa`.`provsmu`,
-                `mahasiswa`.`pekerjaan`,
-                `mahasiswa`.`almt`,
-                `mahasiswa`.`notlp`,
-                `mahasiswa`.`status`,
-                `mahasiswa`.`jmsaudara`,
-                `mahasiswa`.`nmayah`,
-                `mahasiswa`.`almtayah`,
-                `mahasiswa`.`nmibu`,
-                `mahasiswa`.`sumbiaya`,
-                `mahasiswa`.`statuskul`,
-                `mahasiswa`.`tgdaftar`,
-                `mahasiswa`.`kurikulum`
-            FROM
-                `mahasiswa` WHERE npm='$npm' AND statuskul in('AKTIF', 'CUTI', 'TIDAK AKTIF', 'TRANSFER')");
-            if ($ResultMahasiswa->num_rows()) {
-                $data = [
-                    'status' => true,
-                    'data' => $ResultMahasiswa->result(),
-                    'message' => "Success",
-                ];
-                return $data;
-            } else {
-                $data = [
-                    'status' => true,
-                    'data' => $ResultMahasiswa->result(),
-                    'message' => "Tidak Data Mahasiswa",
-                ];
-                return $data;
-            }
-        } else {
-            $ResultMahasiswa = $this->db->query("
-            SELECT
-                `mahasiswa`.`npm`,
-                `mahasiswa`.`kdps`,
-                `mahasiswa`.`jenjang`,
-                `mahasiswa`.`kelas`,
-                `mahasiswa`.`nmmhs`,
-                `mahasiswa`.`tmlhr`,
-                `mahasiswa`.`tglhr`,
-                `mahasiswa`.`jk`,
-                `mahasiswa`.`agama`,
-                `mahasiswa`.`kewarga`,
-                `mahasiswa`.`pendidikan`,
-                `mahasiswa`.`nmsmu`,
-                `mahasiswa`.`jursmu`,
-                `mahasiswa`.`kotasmu`,
-                `mahasiswa`.`kabsmu`,
-                `mahasiswa`.`provsmu`,
-                `mahasiswa`.`pekerjaan`,
-                `mahasiswa`.`almt`,
-                `mahasiswa`.`notlp`,
-                `mahasiswa`.`status`,
-                `mahasiswa`.`jmsaudara`,
-                `mahasiswa`.`nmayah`,
-                `mahasiswa`.`almtayah`,
-                `mahasiswa`.`nmibu`,
-                `mahasiswa`.`sumbiaya`,
-                `mahasiswa`.`statuskul`,
-                `mahasiswa`.`tgdaftar`,
-                `mahasiswa`.`kurikulum`
-            FROM
-            `mahasiswa` WHERE statuskul in('AKTIF', 'CUTI', 'TIDAK AKTIF', 'TRANSFER')");
             if ($ResultMahasiswa->num_rows()) {
                 $data = [
                     'status' => true,
