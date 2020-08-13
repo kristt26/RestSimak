d35ae9ff6033d97522081cf71f7838b359812653
kristt26 <kristt26@gmail.com>
2020-08-13 20:50:57 +0900

    dd

--- application/config/routes.php
+++ application/config/routes.php
@@ -183,4 +183,5 @@ $route['v2/getmahasiswa/:num']['get'] = 'api/v2/Mahasiswa/GetMahasiswa';
 $route['v2/getipk/:num']['get'] = 'api/v2/Mahasiswa/GetIPKMahasiswa';
 $route['v2/getips/:num']['get'] = 'api/v2/Mahasiswa/GetIPSMahasiswa';
 $route['v2/datamahasiswa/:num']['get'] = 'api/v2/Mahasiswa/DataMahasiswa';
+$route['v2/datamahasiswa']['get'] = 'api/v2/Mahasiswa/DataMahasiswa';
 $route['v2/authorization']['post'] = 'api/v2/Users/Login';
