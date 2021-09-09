3c9f3e6c625b4c782fddb6b18c9ed2908f91f01f
kristt26 <kristt26@gmail.com>
2021-09-09 13:42:05 +0900

    jadwal

--- application/models/Jadwal_model.php
+++ application/models/Jadwal_model.php
@@ -256,6 +256,7 @@ class Jadwal_Model extends CI_Model
                         $DataJadwal = array(
                             'message' => 'MulaiReg',
                             'status' => true,
+                            'dataReg'=> $DataTA
                         );
                         return $DataJadwal;
                     }
