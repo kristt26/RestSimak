f36f4e9607cfaa91c3ffaa708ab6c4f53606f692
Ajenkris Kungkung <kristt26@gmail.com>
2022-01-26 15:36:54 +0900

    sd

--- application/controllers/api/Mahasiswa.php
+++ application/controllers/api/Mahasiswa.php
@@ -101,6 +101,10 @@ class Mahasiswa extends \Restserver\Libraries\REST_Controller
     
     public function getadd_get()
     {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json; charset=UTF-8");
+        header("Access-Control-Allow-Methods: POST, GET, DELETE, PUT, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
         $Output = $this->ProdiModel->select();
         $this->response($Output, REST_Controller::HTTP_OK);
     }
