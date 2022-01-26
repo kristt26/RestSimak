2370f620cffdecbf248146975594ea379aaa0012
Ajenkris Kungkung <kristt26@gmail.com>
2022-01-26 22:33:37 +0900

    gd

--- application/models/Prodi_model.php
+++ application/models/Prodi_model.php
@@ -4,7 +4,10 @@ class Prodi_Model extends CI_Model
 {
     public function select()
     {
-        return $this->db->query("SELECT * FROM program_studi")->result_object();
+        $data = $this->db->query("SELECT * FROM program_studi")->result_object();
+        foreach ($data as $key => $value) {
+            $value->kurikulum = $this->db->query("SELECT DISTINCt kurikulum FROM matakuliah WHERE kdps = '$value->kdps' AND kurikulum IS NOT NULL");
+        }
     }
 
 }
