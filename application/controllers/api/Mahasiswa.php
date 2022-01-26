f9afaba33d08e3ced317b915c7656091beed1094
Ajenkris Kungkung <kristt26@gmail.com>
2022-01-26 22:16:27 +0900

    df

--- application/controllers/api/Mahasiswa.php
+++ application/controllers/api/Mahasiswa.php
@@ -60,20 +60,20 @@ class Mahasiswa extends \Restserver\Libraries\REST_Controller
 
     public function AddMahasiswa_post()
     {
-        $this->load->library('Authorization_Token');
-        $is_valid_token = $this->authorization_token->validateToken();
-        if ($is_valid_token['status'] === true) {
-            $data = json_decode($this->security->xss_clean($this->input->raw_input_stream), true);
-            $this->response($data, REST_Controller::HTTP_OK);
-            // $result = $this->MahasiswaModel->addmahasiswa($data);
-            // if ($result !== false) {
-            //     $this->response($result, REST_Controller::HTTP_OK);
-            // } else {
-            //     $this->response($result, REST_Controller::HTTP_BAD_REQUEST);
-            // }
-        } else {
-            $this->response("Token Session Anda kadaluarsa", REST_Controller::HTTP_UNAUTHORIZED);
-        }
+        $this->response('testing', REST_Controller::HTTP_OK);
+        // $this->load->library('Authorization_Token');
+        // $is_valid_token = $this->authorization_token->validateToken();
+        // if ($is_valid_token['status'] === true) {
+        //     $data = json_decode($this->security->xss_clean($this->input->raw_input_stream), true);
+        //     // $result = $this->MahasiswaModel->addmahasiswa($data);
+        //     // if ($result !== false) {
+        //     //     $this->response($result, REST_Controller::HTTP_OK);
+        //     // } else {
+        //     //     $this->response($result, REST_Controller::HTTP_BAD_REQUEST);
+        //     // }
+        // } else {
+        //     $this->response("Token Session Anda kadaluarsa", REST_Controller::HTTP_UNAUTHORIZED);
+        // }
     }
 
     public function MahasiswaProdi_get()
