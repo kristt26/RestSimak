0cceac344b84778a25a75cb5652d5560f3f0f3a5
Ajenkris Kungkung <kristt26@gmail.com>
2022-07-07 12:03:10 +0900

    as

--- application/models/Prodi_model.php
+++ application/models/Prodi_model.php
@@ -13,8 +13,8 @@ class Prodi_Model extends CI_Model
 
 	public function get()
 	{
-		$data['ta'] = $this->db->query("SELECT * FROM tahun_akademik WHERE status='AKTIF'")->result();
-		$data['prodi'] = $this->db->query("SELECT * FROM program_studi WHERE status='true'")->row_object();
+		$data['ta'] = $this->db->query("SELECT * FROM tahun_akademik WHERE status='AKTIF'")->row_object();
+		$data['prodi'] = $this->db->query("SELECT * FROM program_studi WHERE status='true'")->result();
 		$data['dosen'] = $this->db->query("SELECT * FROM dosen")->result_object();
 		$data['mahasiswa'] = $this->db->query("SELECT
 			`mahasiswa`.`npm`,
