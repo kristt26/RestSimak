025617316ab19d9b33950c9080b060e291cc1a18
Ajenkris Kungkung <kristt26@gmail.com>
2022-07-11 09:48:03 +0900

    d

--- application/controllers/api/DosenWali.php
+++ application/controllers/api/DosenWali.php
@@ -19,19 +19,18 @@ class DosenWali extends \Restserver\Libraries\REST_Controller
 
 	public function read_get()
 	{
-		// $data = json_decode($this->security->xss_clean($this->input->raw_input_stream), true);
-		$this->response(["testing" => "Testing"], REST_Controller::HTTP_OK);
-		// $this->load->library('Authorization_Token');
-		// $is_valid_token = $this->authorization_token->validateToken();
-		// if ($is_valid_token['status'] === true) {
-		// 	$result = $this->DosenWaliModel->read($kdps, $iddosen);
-		// 	$message = [
-		// 		'status' => true,
-		// 		'data' => $result,
-		// 		'message' => "Success",
-		// 	];
-		// 	$this->response($message, REST_Controller::HTTP_OK);
-		// }
+		$data = json_decode($this->security->xss_clean($this->input->raw_input_stream), true);
+		$this->load->library('Authorization_Token');
+		$is_valid_token = $this->authorization_token->validateToken();
+		if ($is_valid_token['status'] === true) {
+			// $result = $this->DosenWaliModel->read($kdps, $iddosen);
+			$message = [
+				'status' => true,
+				'data' => $data,
+				'message' => "Success",
+			];
+			$this->response($message, REST_Controller::HTTP_OK);
+		}
 	}
 
 	public function insert_get()
