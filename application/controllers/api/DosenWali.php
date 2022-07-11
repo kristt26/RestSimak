334ee16ee43385c4f4d274141604c60b9de2d9c2
Ajenkris Kungkung <kristt26@gmail.com>
2022-07-11 09:35:14 +0900

    as

--- application/controllers/api/DosenWali.php
+++ application/controllers/api/DosenWali.php
@@ -17,10 +17,10 @@ class DosenWali extends \Restserver\Libraries\REST_Controller
 		// $this->load->model('Dosenwali_model', 'DosenWaliModel');
 	}
 
-	public function read_post()
+	public function read_get()
 	{
-		$data = json_decode($this->security->xss_clean($this->input->raw_input_stream), true);
-		$this->response($data, REST_Controller::HTTP_OK);
+		// $data = json_decode($this->security->xss_clean($this->input->raw_input_stream), true);
+		$this->response(["testing" => "Testing"], REST_Controller::HTTP_OK);
 		// $this->load->library('Authorization_Token');
 		// $is_valid_token = $this->authorization_token->validateToken();
 		// if ($is_valid_token['status'] === true) {
