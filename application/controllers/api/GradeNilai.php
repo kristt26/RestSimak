030d2beaf51c11103da973a2c658578e00692467
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2024-01-03 09:11:57 +0900

    Tanggal Input Nilai

--- application/controllers/api/GradeNilai.php
+++ application/controllers/api/GradeNilai.php
@@ -21,17 +21,25 @@ class GradeNilai extends \Restserver\Libraries\REST_Controller
     {
         
         // $_POST = $this->security->xss_clean($_POST);
-        $this->load->library('Authorization_Token');
-        $is_valid_token = $this->authorization_token->validateToken();
-        if ($is_valid_token['status'] === true) {
-            $result = $this->GradeNilaiModel->get();
-            $message = [
-                'status' => true,
-                'data' => $result,
-                'message' => "Success",
-            ];
-            $this->response($message, REST_Controller::HTTP_OK);
-        }
+		if (strtotime(date("Y/m/d")." 23:59:59") <= strtotime("2024/01/08 23:59:59")) {
+			$this->load->library('Authorization_Token');
+			$is_valid_token = $this->authorization_token->validateToken();
+			if ($is_valid_token['status'] === true) {
+				$result = $this->GradeNilaiModel->get();
+				$message = [
+					'status' => true,
+					'data' => $result,
+					'message' => "Success",
+				];
+				$this->response($message, REST_Controller::HTTP_OK);
+			}
+		}else{
+			$message = [
+				'status' => true,
+				'message' => "Tanggal pengisian berita acara telah ditutup",
+			];
+			$this->response($message, REST_Controller::HTTP_BAD_REQUEST);
+		}
     }
 
 }
